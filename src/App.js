8c53e502e2864892ef0d1644f1cb3950fb816b1e
andrewnguyen22 <amnguyen1@mail.usf.edu>
2019-01-30 11:25:42 -0500

    added attribute and reference components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, {Component} from 'react'
 import './App.css'
 import axios from 'axios'
+import Reference from './components/Reference'
 
 class App extends Component {
 
     constructor() {
         super();
         this.state = {
-            relayRoutes: '',
-            clientRoutes: ''
+            relayRoutes: [],
+            clientRoutes: []
         };
         this.get = this.get.bind(this)
     }
 
     get() {
         axios.get('http://localhost:8081/v1/routes')
-            .then(response => this.setState({relayRoutes: response.data}))
+            .then(response => this.setState({relayRoutes: response.data}));
         axios.get('http://localhost:8080/v1/routes')
             .then(response => this.setState({clientRoutes: response.data}))
     }
@@ -25,11 +26,20 @@ class App extends Component {
     }
 
     render() {
+        const RelayHost = "http://localhost:8081";
+        const ClientHost = "http://localhost:8080";
+        const RelayRef = this.state.relayRoutes.map((val) => <Reference url={RelayHost + val}/>);
+        const ClientRef = this.state.clientRoutes.map((val) => <Reference url={ClientHost + val}/>);
         return (
-            <div className='button__container'>
-                <p>{JSON.stringify(this.state.relayRoutes, null, 2)}</p>
-                <br />
-                <p>{JSON.stringify(this.state.clientRoutes, null, 2)}</p>
+            <div className='reference-body'>
+                <h1>Relay API Reference</h1>
+                <div className={'reference-container'}>
+                    {RelayRef}
+                </div>
+                <h1>Client API Reference</h1>
+                <div className={'reference-container'}>
+                    {ClientRef}
+                </div>
             </div>
         )
     }
